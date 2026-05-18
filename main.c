5e4b99655fc9a612c1682bdd9e51734baec38189
Nahihy <sagivavri@gmail.com>
2026-05-18 07:48:27 +0300

    H

--- main.c
+++ main.c
@@ -6,7 +6,7 @@ int main() {
   randererSetAutoFrameResizingKeepRatio(&randerer);
 
   World world = createWorld(createBackground("purpleMountsBackground.jpg", GL_RGB, 0.0f, 1.0f, 1.0f, 0.0f, BG_REPEAT),
-                            8, (int[2]){0, 5}, (float[2]){0.0f, 0.1f}, 0.1f);
+                            8, (int[2]){0, 5}, (float[2]){0.0f, 0.1f}, 1.0f);
   
   groundAdd(&world.ground, "container.jpg", GL_RGB, GO_SQUARE,  1.0f,  0.0f, 0.2f, 0.2f, 0.0f);
   groundAdd(&world.ground, "container.jpg", GL_RGB, GO_SQUARE, -1.0f,  0.0f, 0.2f, 0.2f, 0.0f);
@@ -14,7 +14,7 @@ int main() {
   for(float i = -1.0f; i < 10.0f; i += 0.4f) 
     groundAdd(&world.ground, "container.jpg", GL_RGB, GO_SQUARE, i, -0.4f, 0.2f, 0.2f, 0.0f);
 
-  Player player = createPlayer("mario.png", GL_RGBA, 20, 0.5f, 0.1f, 1.0f, (float[2]){0.3333f, 0.3333f}, (TexColumn){0, 1, EN_REPEAT},
+  Player player = createPlayer("mario.png", GL_RGBA, 20, 0.5f, 0.1f, 0.5f, (float[2]){0.3333f, 0.3333f}, (TexColumn){0, 1, EN_REPEAT},
                                (TexColumn){1, 3, EN_REPEAT}, (TexColumn){2, 1, EN_REPEAT}, 0.0f, 0.01f, 0.2f, 0.2f);
   glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
   glEnable(GL_BLEND);
