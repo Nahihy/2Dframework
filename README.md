74b6b4c66f7d76d15df35603413c01f1c6f7eb68
Nahihy <sagivavri@gmail.com>
2026-06-01 14:28:35 +0300

    updated readme

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 My final project for my uni course.
-It's a lib to easily create 2D graphical games, plus a randerer lib to make base OpenGL easier which the framework is based on.
+It's a lib to easily create 2D graphical games, based on a randerer lib to make base (2D) OpenGL easier and object orianted and a datatbase lib to save data on files.
 both the libs are made with only c and cmake and those are the tools needed to compile it.
-(also everything except the 2Dframework folder is mainly for testing, and temporarely needing libglfw3 which is added in the nix shell(I'm using a non FHS complient linux distro(NixOS) and glfw's source doesn't work with it except the nix package)).
-
+the master branch is for unix/unix-like systems (linux/bsd/macos) and windows branch for windows.
--- libTest.c
+++ libTest.c
@@ -31,12 +31,12 @@ int main() {
     groundAdd(&world.ground, "brick.png", GL_RGBA, GO_SQUARE, 15.0f, i, 0.1f, 0.1f, 0.0f);
   for(int i = 1; i < 30; i++) {
     for(int j = 1; j <= i; j++) {
-      // groundAdd(&world.ground, "brick.png", GL_RGBA, GO_SQUARE, 0.6f + 0.4f * i, 0.2f * j - 1.0f, 0.1f, 0.1f, 0.0f);
+      groundAdd(&world.ground, "brick.png", GL_RGBA, GO_SQUARE, 0.6f + 0.4f * i, 0.2f * j - 1.0f, 0.1f, 0.1f, 0.0f);
     }
   }
   
 
-  Player player = createPlayer("mario.png", GL_RGBA, 20, 0.5f, 0.4f, 2.2f, (float[2]){0.3333f, 0.3333f},
+  Player player = createPlayer("mario.png", GL_RGBA, 20, 0.5f, 0.04f, 0.2f, (float[2]){0.3333f, 0.3333f},
                                (TexColumn){0, 1, EN_REPEAT}, (TexColumn){1, 3, EN_REPEAT},
                                (TexColumn){2, 1, EN_REPEAT}, 0.0f, 0.01f, 0.15f, 0.15f);
   glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
