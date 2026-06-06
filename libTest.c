ccf89a0fb554f79bdd5abb21843e849c648ae06d
Nahihy <sagivavri@gmail.com>
2026-06-06 19:31:46 +0300

    changed test world

--- libTest.c
+++ libTest.c
@@ -23,22 +23,18 @@ int main() {
   
   groundAdd(&world.ground, "brick.png", GL_RGBA, GO_SQUARE,  1.0f,  -0.6f, 0.1f, 0.1f, 0.0f);
   groundAdd(&world.ground, "brick.png", GL_RGBA, GO_SQUARE, -1.0f,  2.0f, 0.1f, 0.1f, 0.0f);
-  for(float i = -15.0f; i < 15.0f; i += 0.2f) 
+  for(float i = -15.0f; i < 1.0f; i += 0.2f) 
     groundAdd(&world.ground, "brick.png", GL_RGBA, GO_SQUARE, i, -0.8f, 0.1f, 0.1f, 0.0f);
-  // for(float i = -0.8f; i < 5.0f; i += 0.2f) 
-  //   groundAdd(&world.ground, "brick.png", GL_RGBA, GO_SQUARE, -15.0f, i, 0.1f, 0.1f, 0.0f);
-  // for(float i = -0.8f; i < 5.0f; i += 0.2f) 
-  //   groundAdd(&world.ground, "brick.png", GL_RGBA, GO_SQUARE, 15.0f, i, 0.1f, 0.1f, 0.0f);
-  for(int i = 1; i < 30; i++) {
-    for(int j = 1; j <= i; j++) {
+  for(float i = -0.8f; i < 5.0f; i += 0.2f) 
+    groundAdd(&world.ground, "brick.png", GL_RGBA, GO_SQUARE, -15.0f, i, 0.1f, 0.1f, 0.0f);
+  for(int i = 1; i < 30; i++)
+    for(int j = 1; j <= i; j++)
       groundAdd(&world.ground, "brick.png", GL_RGBA, GO_SQUARE, 0.6f + 0.4f * i, 0.2f * j - 1.0f, 0.1f, 0.1f, 0.0f);
-    }
-  }
   
 
   Player player = createPlayer("mario.png", GL_RGBA, 20, 0.5f, 0.04f, 0.2f, (float[2]){0.3333f, 0.3333f},
                                (TexColumn){0, 1, EN_REPEAT}, (TexColumn){1, 3, EN_REPEAT},
-                               (TexColumn){2, 1, EN_REPEAT}, 0.0f, 0.01f, 0.15f, 0.15f);
+                               (TexColumn){2, 1, EN_REPEAT}, 0.0f, 0.01f, 0.1f, 0.1);
   glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
   glEnable(GL_BLEND);
 
